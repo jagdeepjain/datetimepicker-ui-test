d6d1a22ae0ede1a8eba927b8417d6bf428cc59ee
jagdeepjain <jagdeep.jain@gmail.com>
2014-09-13 19:02:17 +0530

    Update XPaths.java
    updated xpaths of hr and minute

--- src/main/java/org/datetimepicker/ui/components/XPaths.java
+++ src/main/java/org/datetimepicker/ui/components/XPaths.java
@@ -11,8 +11,8 @@ public class XPaths {
 
 	// date picker
 	public static final String calMonth = "//*[@id='ui-datepicker-div']/div[1]/a[2]/span";
-	public static final String calHr = "//*[@id='ui-datepicker-div']/div[2]/dl/dd[2]/div/a";
-	public static final String calMinute = "//*[@id='ui-datepicker-div']/div[2]/dl/dd[3]/div";
+	public static final String calHr = "//*[@id='ui-datepicker-div']/div[2]/dl/dd[2]/div/span";
+	public static final String calMinute = "//*[@id='ui-datepicker-div']/div[2]/dl/dd[3]/div/span";
 	public static final String datePickerDone = "//*[@id='ui-datepicker-div']/div[3]/button[2]";
 	public static final String datePickerNow = "//*[@id='ui-datepicker-div']/div[3]/button[1]";
 
