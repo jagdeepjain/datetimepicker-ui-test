ece6aa87caf9e69c7d7c9bf5bd7014b9f4ba352a
Jagdeep Jain <jagdeepjain@jagdeepjain.local>
2015-03-23 01:29:11 +0530

    added seperate package for test assertions

--- src/test/java/org/datetimepicker/ui/tests/TimePickerBasicExample1Test.java
+++ src/test/java/org/datetimepicker/ui/tests/TimePickerBasicExample1Test.java
@@ -6,16 +6,18 @@ package org.datetimepicker.ui.tests;
 
 import org.datetimepicker.examples.TimePickerExample;
 import org.datetimepicker.ui.common.AutomationUtil;
-import org.datetimepicker.ui.common.Assertion;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.ui.test.assertions.Assertion;
 
 public class TimePickerBasicExample1Test extends BaseTest {
 
 	TimePickerExample timePickerBasicExample1 = new TimePickerExample();
 	AutomationUtil automationUtil = new AutomationUtil();
 	Assertion assertTest = new Assertion();
+	private String actual;
+	private String expected;
 
 	@Before
 	public void setUp() throws Exception {
@@ -33,9 +35,10 @@ public class TimePickerBasicExample1Test extends BaseTest {
 		timePickerBasicExample1.clickExamplesLink(driver);
 		// next month from current date on 10th at 9:00 AM
 		timePickerBasicExample1.setTimeStamp(driver, 1, 9, 0, "10");
-		assertTest.trueEquals(automationUtil.datePickedByUser(1, 10, 9, 0),
-						driver, className);
-
+		// converting to date format for comparison
+		expected = automationUtil.datePickedByUser(1, 10, 9, 0);
+		actual = timePickerBasicExample1.getTimeStamp(driver);
+		assertTest.trueEquals(expected, actual,driver, className);
 	}
 
 }
