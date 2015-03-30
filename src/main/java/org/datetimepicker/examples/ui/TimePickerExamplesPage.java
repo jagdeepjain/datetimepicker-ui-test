9479c82cfd2ea08daa69c5c07bc9c580dd5c069e
Jagdeep Jain <jagdeep.jain@gmail.com>
2015-03-30 09:49:29 +0530

    removed locator from WebElement variables

--- src/main/java/org/datetimepicker/examples/ui/TimePickerExamplesPage.java
+++ src/main/java/org/datetimepicker/examples/ui/TimePickerExamplesPage.java
@@ -6,70 +6,69 @@ package org.datetimepicker.examples.ui;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class TimePickerExamplesPage {
   // locators
   @FindBy(tagName = "h1")
-  protected WebElement pageTitleLocator;
+  protected WebElement pageTitle;
   @FindBy(id = "ui-id-5")
-  protected WebElement examplesLinkLocator;
+  protected WebElement examplesLink;
   @FindBy(id = "basic_example_1")
-  protected WebElement basicExamplesLinkLocator;
-  
+  protected WebElement basicExamplesLink;
   @FindBy(id = "ui-datepicker-div")
-  protected WebElement dateTimePickerLocator;
- 
+  protected WebElement dateTimePicker;
   protected By dateTimePickerMonthLocator = By.linkText("Next");
   protected By dateTimePickerDateLocator = By.tagName("td");
   protected By dateTimePickerHourMinuteLocator = By.className("ui-slider-handle");
   protected By dateTimePickerDoneLocator = By.className("ui-datepicker-close");
-  
+
   // get page title
   public String getPageTitle() {
-    return pageTitleLocator.getText();
+    return pageTitle.getText();
   }
   
   // click on examples tab
   public TimePickerExamplesPage clickExamplesTab() {
-    examplesLinkLocator.click();
+    examplesLink.click();
+    
+    // returning to the same page
     return this;
   }
   
   // use calendar to set date and time
   public TimePickerExamplesPage setDateTime(int month, int selectedDate, int startHour, int startMinute) {
     // show calendar
-    basicExamplesLinkLocator.click();
+    basicExamplesLink.click();
     
     // set month
     for (int i = 1; i <= month; i++) {
-      dateTimePickerLocator.findElement(dateTimePickerMonthLocator).click();
+      dateTimePicker.findElement(dateTimePickerMonthLocator).click();
     }
     // set date
-    int size = dateTimePickerLocator.findElements(dateTimePickerDateLocator).size();
+    int size = dateTimePicker.findElements(dateTimePickerDateLocator).size();
     for (int i = 1; i <= size; i++) {
-      String text = dateTimePickerLocator.findElements(dateTimePickerDateLocator).get(i).getText();
+      String text = dateTimePicker.findElements(dateTimePickerDateLocator).get(i).getText();
       int dateTimePickerDateLocatorText = 0;
       if (!" ".equals(text)) {
         dateTimePickerDateLocatorText = Integer.valueOf(text);
         if  (dateTimePickerDateLocatorText == selectedDate) {
-          dateTimePickerLocator.findElements(dateTimePickerDateLocator).get(i).click();
+          dateTimePicker.findElements(dateTimePickerDateLocator).get(i).click();
           break;
         }
       }
     }
     // set hour
     for (int i = 1; i <= startHour; i++) {
-      dateTimePickerLocator.findElements(dateTimePickerHourMinuteLocator).get(0).sendKeys(Keys.ARROW_RIGHT);
+      dateTimePicker.findElements(dateTimePickerHourMinuteLocator).get(0).sendKeys(Keys.ARROW_RIGHT);
     }
     // set minute
     for (int i = 1; i <= startMinute; i++) {
-      dateTimePickerLocator.findElements(dateTimePickerHourMinuteLocator).get(1).sendKeys(Keys.ARROW_RIGHT);
+      dateTimePicker.findElements(dateTimePickerHourMinuteLocator).get(1).sendKeys(Keys.ARROW_RIGHT);
     }
     // clicking on done button
-    dateTimePickerLocator.findElement(dateTimePickerDoneLocator).click();
+    dateTimePicker.findElement(dateTimePickerDoneLocator).click();
     
     // returning on the same page as date picker does not navigate to any page
     return this;
@@ -77,7 +76,7 @@ public class TimePickerExamplesPage {
   
   // get the entered date and time
   public String getDateTimeValue() {
-    return basicExamplesLinkLocator.getAttribute("value");
+    return basicExamplesLink.getAttribute("value");
   }
   
 }
